71583a6e0d3c5bd567a5f2338354296226cc46ce
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-11-06 21:39:10 +0300

    setup.py: add project description and deps

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from os import path
+
+
+PACKAGE_NAME = 'nameko_worker_logger'
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name=PACKAGE_NAME,
+    version='0.0.1',
+    description='A package with logger for nameko worker',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/emlid/Nameko-logging-ELK',
+    author='Emlid ltd.',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=[PACKAGE_NAME],
+    install_requires=[
+        'nameko==2.11.0'
+    ],
+)
